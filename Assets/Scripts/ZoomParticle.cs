70871193388d7bcc517793c23c87e22264c2bced
Seth Gibson <seth.gibson@intel.com>
2014-10-30 16:57:23 -0700

    project rearrangement -sG

---
+++ Assets/Scripts/ZoomParticle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomParticle : MonoBehaviour
+{
+    public float Speed;
+    private bool mIsDead;
+
+    public bool IsDead() { return mIsDead; }
+	void Start ()
+    {
+        mIsDead = false;
+	}
+	
+	void Update ()
+    {
+        Vector3 nP = new Vector3(0, 0, transform.position.z - Speed);
+        transform.position = nP;
+        float cAlpha = Mathf.Lerp(0, 1, nP.z / 10.0f);
+        foreach(Renderer m in GetComponentsInChildren<MeshRenderer>())
+        {
+            m.material.color = new Color(1, 1, 1, cAlpha);
+        }
+
+        if(transform.position.z <= 0)
+        {
+            mIsDead = true;
+        }
+	}
+}
